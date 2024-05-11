84fc58fdc0ac7989a2bffb9644e0763ffb9ac9b9
williansql <willslipknotsp@gmail.com>
2024-05-11 11:26:53 -0300

    first commit

---
+++ src/main/java/com/foffaps/estoquesimples/EstoqueSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.foffaps.estoquesimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foffaps/estoquesimples/EstoqueSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foffaps.estoquesimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
